d2abb169c51f690c0f9b99e92a725e9d26d81756
wMw <wmwofficial@gmail.com>
2018-07-20 17:23:22 +0300

    0

--- olyasha-telegram-bot/olyasha-telegram-bot.py
+++ olyasha-telegram-bot/olyasha-telegram-bot.py
@@ -44,7 +44,7 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help, Filters.user(username='@wmwofficial')))
+    dp.add_handler(CommandHandler("help", help, filters=Filters.user(username='@wmwofficial')))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
