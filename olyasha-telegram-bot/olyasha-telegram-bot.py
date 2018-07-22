d032064e8b2cd58194f0e8478b226cdec653b9be
wMw <wmwofficial@gmail.com>
2018-07-23 00:44:30 +0300

    0

--- olyasha-telegram-bot/olyasha-telegram-bot.py
+++ olyasha-telegram-bot/olyasha-telegram-bot.py
@@ -1,64 +1,107 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import time
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+# from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 import tkn
 
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
-    """Send a message when the command /start is issued."""
+    print(update.message.text)
     update.message.reply_text('Hi!')
+
+def new_member(bot, update):
+    print(update.message.text)
+    update.message.reply_sticker(sticker='CAADAgADaQADk5h-FwzA5kpuP7CvAg')
 
 
 def help(bot, update):
-    """Send a message when the command /help is issued."""
     update.message.reply_text('Help! | ' + str(update.message.chat_id))
 
 
-def echo(bot, update):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text  + str(update.message.chat_id))
+def mem(bot, update, args):
+    print(update.message.text)
+    if not args:
+        # bot.send_chat_action(chat_id=update.message.chat_id, action='typing')
+        # kb = [[telegram.KeyboardButton('/mem дуранчоусы')],
+        #       [telegram.KeyboardButton('/mem тест')],
+        #       [telegram.KeyboardButton('/mem тест')],
+        #       [telegram.KeyboardButton('/mem тест')],
+        #       [telegram.KeyboardButton('/mem тест')],
+        #       [telegram.KeyboardButton('/mem тест')],]
+        # kb_markup = telegram.ReplyKeyboardMarkup(kb, resize_keyboard=True, one_time_keyboard=True, selective=True)
+        # print(update.message.message_id)
+        # print(update.message.from_user)
+        # print(update.message.from_user['first_name'])
+        # bot.send_message(chat_id=update.message.chat_id,
+        #                 text='<a href="tg://user?id=%s">%s</a> Мемы:\n'
+        #                 '/mem дуранчоусы — Вы шо дуранчоусы?\n'
+        #                 '/mem тест — тест' % (update.message.from_user['id'], update.message.from_user['first_name']), parse_mode='HTML',
+        #                  reply_markup=kb_markup)
+        # kb = [[InlineKeyboardButton('дуранчоусы', callback_data='дуранчоусы'),
+        #        InlineKeyboardButton('похотливый_борис', callback_data='2')],
+        #       [InlineKeyboardButton('Option 3', callback_data='3')]]
+        # kb_markup = InlineKeyboardMarkup(kb)
+        bot.send_message(chat_id=update.message.chat_id,
+                        text='Мемы:\n'
+                        '/mem дуранчоусы  — <i>Вы шо дуранчоусы?</i>\n'
+                        '/mem борис  — <i>Похотливый Борис</i>', parse_mode='HTML')
+    for s in args:
+        if s.lower() == 'дуранчоусы':
+            bot.send_chat_action(chat_id=update.message.chat_id, action='record_video_note')
+            bot.send_video_note(chat_id=update.message.chat_id, video_note='DQADAgADUAIAAjpVqUoP0y7cItR_swI')
+            # bot.send_video_note(chat_id=update.message.chat_id, video_note=open('files/duranchousi.mp4', 'rb'),
+            #                     length='352', duration=1)
+        elif s.lower() == 'борис':
+            bot.send_chat_action(chat_id=update.message.chat_id, action='record_video_note')
+            bot.send_video_note(chat_id=update.message.chat_id, video_note='DQADAgADWwIAAoCFUUqenc0y23OmGgI')
+
+
+def mem_btn(bot, update):
+    q = update.callback_query
+    # bot.edit_message_text(text='Selected: {}'.format(q.data),
+    #                       chat_id=q.message.chat_id,
+    #                       message_id=q.message.message_id)
+    if q.data == 'дуранчоусы':
+        bot.send_chat_action(chat_id=q.message.chat_id, action='record_video_note')
+        bot.delete_message(chat_id=q.message.chat_id, message_id=q.message.message_id)
+        # bot.edit_message_text(text='/meme {}'.format(q.data),
+        #                                             chat_id=q.message.chat_id,
+        #                                             message_id=q.message.message_id)
+        bot.send_video_note(chat_id=q.message.chat_id, video_note=open('files/duranchousi.mp4', 'rb'), \
+                        length='352', duration=1)
+
+
+def getchatid(bot, update):
+    update.message.reply_text(str(update.message.chat_id))
 
 
 def error(bot, update, error):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 def main():
-    """Start the bot."""
-    # Create the EventHandler and pass it your bot's token.
     updater = Updater(tkn.BOT_TKN)
-
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
+    dp.add_handler(CallbackQueryHandler(mem_btn))
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help, filters=Filters.user(user_id=1441490771) | Filters.user(user_id=4044957411) | Filters.chat(chat_id=-1001289154724)))
+    dp.add_handler(CommandHandler("mem", mem, pass_args=True))
+    # dp.add_handler(CommandHandler("getchatid", getchatid))
+    dp.add_handler(CommandHandler("getchatid", getchatid, filters=Filters.user(user_id=144149077)))
+    dp.add_handler(CommandHandler("help", help, filters=Filters.user(user_id=144149077) | \
+                                                        Filters.user(user_id=4044957411)| \
+                                                        Filters.chat(chat_id=-1001289154724)))
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-    #dp.add_handler(MessageHandler(Filters.chat(-144149077) & Filters.text, echo))
-
-    # log all errors
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
+    # dp.add_handler(MessageHandler(Filters.chat(-144149077) & Filters.text, echo))
     dp.add_error_handler(error)
-
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
